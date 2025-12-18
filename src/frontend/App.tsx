647f2772f0fa138796d4d8bdf15cf4c6b6e09fe0
firesquid6 <jdeiss06@gmail.com>
2025-12-18 15:11:18 -0600

    clean out some boilerplate

---
+++ src/frontend/App.tsx
@@ -0,0 +1,12 @@
+import "./main.css";
+
+
+export function App() {
+  return (
+    <div className="max-w-7xl mx-auto p-8 text-center relative z-10">
+      <p>Hello, world!</p>
+    </div>
+  );
+}
+
+export default App;
